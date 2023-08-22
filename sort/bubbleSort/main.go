ab5fad777ca798acef92cc50c71ad851fbaa6cb4
sunmeng <1241577140@qq.com>
2023-08-22 11:39:29 +0800

    [update] 2023/8/22

--- sort/bubbleSort/main.go
+++ sort/bubbleSort/main.go
@@ -10,7 +10,7 @@ func main() {
 
 func bubbleSort(arr []int) {
 	for i := 0; i < len(arr)-1; i++ {
-		for j := 0; j < len(arr)-1-i; j++ {
+		for j := 0; j < len(arr)-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
--- sort/quickSort/main.go
+++ sort/quickSort/main.go
@@ -1,52 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
 	nums := []int{5, 2, 3, 1}
-	arr := quick(nums)
-	fmt.Println(arr)
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] < nums[j]
-	})
+	//arr := quick(nums)
+	//fmt.Println(arr)
+	//sort.Slice(nums, func(i, j int) bool {
+	//	return nums[i] < nums[j]
+	//})
+	quickSort(nums, 0, len(nums)-1)
+	fmt.Println(nums)
 }
 
 // 快速排序入口函数
-func quickSort(nums []int, p int, r int) {
+func quickSort(nums []int, l int, r int) {
 	// 递归终止条件
-	if p >= r {
+	if l >= r {
 		return
 	}
-	// 获取分区位置
-	q := partition(nums, p, r)
-	// 递归分区（排序是在定位 pivot 的过程中实现的）
-	quickSort(nums, p, q-1)
-	quickSort(nums, q+1, r)
-}
-
-// 定位 pivot
-func partition(nums []int, p int, r int) int {
-	// 以当前数据序列最后一个元素作为初始 pivot
-	pivot := nums[r]
-	// 初始化 i、j 下标
-	i := p
-	// 后移 j 下标的遍历过程
-	for j := p; j < r; j++ {
-		// 将比 pivot 小的数丢到 [p...i-1] 中，剩下的 [i...j] 区间都是比 pivot 大的
-		if nums[j] < pivot {
-			// 互换 i、j 下标对应数据
-			nums[i], nums[j] = nums[j], nums[i]
-			// 将 i 下标后移一位
+	p := nums[l]
+	i, j := l, r
+	for i < j {
+		for i < j && p >= nums[j] {
+			j--
+		}
+		for i < j && p <= nums[i] {
 			i++
 		}
+		nums[i], nums[j] = nums[j], nums[i]
 	}
-
-	// 最后将 pivot 与 i 下标对应数据值互换
-	// 这样一来，pivot 就位于当前数据序列中间，i 也就是 pivot 值对应的下标
-	nums[i], nums[r] = pivot, nums[i]
-	// 返回 i 作为 pivot 分区位置
-	return i
+	nums[i], nums[r] = nums[r], nums[i]
+	quickSort(nums, l, i-1)
+	quickSort(nums, i+1, r)
 }
